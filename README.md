fc63f87852fef019d22c2400bcb60fe27a8d3a15
Nick Sharp <nsharp@cs.cmu.edu>
2019-07-17 22:39:17 -0400

    initial version of example

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# libigl-polyscope-project-template
-An example project and build system using libIGL and Polyscope
+# libIGL & Polyscope Example Project
+
+Demonstrates basic functionality and project setup for using Polyscope with libIGL.
+
+IGL code mimicks examples from the IGL tutorials.
+
+
+## To download and build
+
+```
+git clone --recurse-submodules https://github.com/nmwsharp/libigl-polyscope-project-template.git
+cd libigl-polyscope-project-template
+mkdir build
+cd build
+cmake ..
+make -j4
+
+./bin/libIGL-polyscope-example ../bunnyhead.obj
+
+./bin/libIGL-polyscope-example ../spot.obj
+```
+
+## Usage
+
+After running the demo appliation as above, try clicking around the command UI in the upper right corner.
+
+Notice that as quantities are added to Polyscope, they appear in the selection window on the left.
+
+Try clicking a vertex to see the quantities associated with that vertex. Clicking may be easier if you first show edge by dragging the "edge width" slider in the left panel.
+
+Check out `src/main.cpp` to see how easy it is! There are only 12 lines of Polyscope code in the whole demo to generate this visualization.
---
+++ src/main.cpp
@@ -0,0 +1,178 @@
+#include "polyscope/polyscope.h"
+
+#include <igl/PI.h>
+#include <igl/avg_edge_length.h>
+#include <igl/barycenter.h>
+#include <igl/boundary_loop.h>
+#include <igl/exact_geodesic.h>
+#include <igl/gaussian_curvature.h>
+#include <igl/invert_diag.h>
+#include <igl/lscm.h>
+#include <igl/massmatrix.h>
+#include <igl/per_vertex_normals.h>
+#include <igl/readOBJ.h>
+
+#include "polyscope/messages.h"
+#include "polyscope/point_cloud.h"
+#include "polyscope/surface_mesh.h"
+
+#include <iostream>
+#include <unordered_set>
+#include <utility>
+
+#include "args/args.hxx"
+#include "json/json.hpp"
+
+// The mesh, Eigen representation
+Eigen::MatrixXd meshV;
+Eigen::MatrixXi meshF;
+
+// Options for algorithms
+int iVertexSource = 7;
+
+void addCurvatureScalar() {
+  using namespace Eigen;
+  using namespace std;
+
+  VectorXd K;
+  igl::gaussian_curvature(meshV, meshF, K);
+  SparseMatrix<double> M, Minv;
+  igl::massmatrix(meshV, meshF, igl::MASSMATRIX_TYPE_DEFAULT, M);
+  igl::invert_diag(M, Minv);
+  K = (Minv * K).eval();
+
+  polyscope::getSurfaceMesh("input mesh")
+      ->addVertexScalarQuantity("gaussian curvature", K,
+                                polyscope::DataType::SYMMETRIC);
+}
+
+void computeDistanceFrom() {
+  Eigen::VectorXi VS, FS, VT, FT;
+  // The selected vertex is the source
+  VS.resize(1);
+  VS << iVertexSource;
+  // All vertices are the targets
+  VT.setLinSpaced(meshV.rows(), 0, meshV.rows() - 1);
+  Eigen::VectorXd d;
+  igl::exact_geodesic(meshV, meshF, VS, FS, VT, FT, d);
+
+  polyscope::getSurfaceMesh("input mesh")
+      ->addVertexDistanceQuantity(
+          "distance from vertex " + std::to_string(iVertexSource), d);
+}
+
+void computeParameterization() {
+  using namespace Eigen;
+  using namespace std;
+
+  // Fix two points on the boundary
+  VectorXi bnd, b(2, 1);
+  igl::boundary_loop(meshF, bnd);
+
+  if (bnd.size() == 0) {
+    polyscope::warning("mesh has no boundary, cannot parameterize");
+    return;
+  }
+
+  b(0) = bnd(0);
+  b(1) = bnd(round(bnd.size() / 2));
+  MatrixXd bc(2, 2);
+  bc << 0, 0, 1, 0;
+
+  // LSCM parametrization
+  Eigen::MatrixXd V_uv;
+  igl::lscm(meshV, meshF, b, bc, V_uv);
+
+  polyscope::getSurfaceMesh("input mesh")
+      ->addVertexParameterizationQuantity("LSCM parameterization", V_uv);
+}
+
+void computeNormals() {
+  Eigen::MatrixXd N_vertices;
+  igl::per_vertex_normals(meshV, meshF, N_vertices);
+
+  polyscope::getSurfaceMesh("input mesh")
+      ->addVertexVectorQuantity("libIGL vertex normals", N_vertices);
+}
+
+void callback() {
+
+  static int numPoints = 2000;
+  static float param = 3.14;
+
+  ImGui::PushItemWidth(100);
+
+  // Curvature
+  if (ImGui::Button("add curvature")) {
+    addCurvatureScalar();
+  }
+  
+  // Normals 
+  if (ImGui::Button("add normals")) {
+    computeNormals();
+  }
+
+  // Param
+  if (ImGui::Button("add parameterization")) {
+    computeParameterization();
+  }
+
+  // Geodesics
+  if (ImGui::Button("compute distance")) {
+    computeDistanceFrom();
+  }
+  ImGui::SameLine();
+  ImGui::InputInt("source vertex", &iVertexSource);
+
+  ImGui::PopItemWidth();
+}
+
+int main(int argc, char **argv) {
+  // Configure the argument parser
+  args::ArgumentParser parser("A simple demo of Polyscope with libIGL.\nBy "
+                              "Nick Sharp (nsharp@cs.cmu.edu)",
+                              "");
+  args::Positional<std::string> inFile(parser, "mesh", "input mesh");
+
+  // Parse args
+  try {
+    parser.ParseCLI(argc, argv);
+  } catch (args::Help) {
+    std::cout << parser;
+    return 0;
+  } catch (args::ParseError e) {
+    std::cerr << e.what() << std::endl;
+
+    std::cerr << parser;
+    return 1;
+  }
+
+  // Options
+  polyscope::options::autocenterStructures = true;
+  polyscope::view::windowWidth = 1024;
+  polyscope::view::windowHeight = 1024;
+
+  // Initialize polyscope
+  polyscope::init();
+
+  std::string filename = args::get(inFile);
+  std::cout << "loading: " << filename << std::endl;
+
+  // Read the mesh
+  igl::readOBJ(filename, meshV, meshF);
+
+  // igl::opengl::glfw::Viewer viewer;
+  // viewer.data().set_mesh(meshV, meshF);
+  // viewer.launch();
+
+  // Register the mesh with Polyscope
+  polyscope::registerSurfaceMesh("input mesh", meshV, meshF);
+
+  // Add the callback
+  polyscope::state::userCallback = callback;
+
+  // Show the gui
+  polyscope::show();
+
+  return 0;
+}
